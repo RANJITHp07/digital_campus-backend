859cb64ffdb2eacd4684c7f8076a589640649b2b
ranjith <ranjithp27122003@gmail.com>
2023-12-27 10:01:19 +0530

    feat(assignment-srv):created comment dependency injection file

--- assignment/src/infrastructureLayer/graphql/injection/comment.ts
+++ assignment/src/infrastructureLayer/graphql/injection/comment.ts
@@ -1,4 +1,13 @@
+import { ErrorHandler } from "../../../../../classroom/src/infrastructureLayer/middleware/error/userErrorhandler";
+import { CommentAdapter } from "../../../adapterLayer/commentAdapter";
+import { Commentusecase } from "../../../usecaseLayer/usecase/commentusecase";
+import { CommentRepository } from "../../repository/queries/commentRepository";
 
 
 //factory pattern
-const repository=
+const repository=new CommentRepository();
+const errorHandler=new ErrorHandler()
+const usecase=new Commentusecase(repository,errorHandler);
+const controller=new CommentAdapter(usecase,errorHandler);
+
+export {controller, errorHandler}
