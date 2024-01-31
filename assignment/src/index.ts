5d01637c7ef26753d9e4a4394d87c2ab5f6cc7b5
Ranjth P <ranjithp27122003@gmail.com>
2024-01-31 12:22:47 +0530

    fix:solved the port problem

--- assignment/src/index.ts
+++ assignment/src/index.ts
@@ -22,7 +22,7 @@ const bootstrap = async () => {
     const app = await createServer();
 
     db().then(() => {
-      app?.listen(4000, () => {
+      app?.listen(PORT  , () => {
         console.log(`Server running on port ${PORT}`);
       });
     });
